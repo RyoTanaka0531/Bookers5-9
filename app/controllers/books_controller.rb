8260f6605f02c9c2076035d1ed3d9563aeb73a36
tanakaryo <ch5cxcxxh@gmail.com>
2020-03-25 21:19:06 +0900

    [Update]5-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,8 +3,11 @@ class BooksController < ApplicationController
     def create
         @book = Book.new(book_params)
         @book.user_id = current_user.id
-        @book.save
-        redirect_to books_path
+         if @book.save
+            redirect_to book_path(@book.id)
+         else
+            render :index
+         end
     end
 
     def index
@@ -15,12 +18,23 @@ class BooksController < ApplicationController
 
     def show
         @book = Book.find(params[:id])
+        @user = @book.user
+        @book_new=Book.new
     end
 
     def destroy
+        @book = Book.find(params[:id])
+        @book.destroy
+        redirect_to books_path
     end
 
     def edit
+        @book = Book.find(params[:id])
+    end
+    def update
+        @book = Book.find(params[:id])
+        @book.update(book_params)
+        redirect_to books_path
     end
 
 
--- db/migrate/20200324031212_remove_opinion_from_book.rb
+++
@@ -1,5 +0,0 @@
-class RemoveOpinionFromBook < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :books, :opinion, :integer
-  end
-end
