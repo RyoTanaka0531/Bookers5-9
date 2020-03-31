5bc80cab0ffcaf7d96c7a72364d5a8324ad4966f
tanakaryo <ch5cxcxxh@gmail.com>
2020-03-31 18:16:42 +0900

    [Finish]5-9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,23 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
-    
     before_action :configure_permitted_parameters, if: :devise_controller?
+    protect_from_forgery with: :exception
 
-    protected
-    def configure_permitted_parameters
-        devise_parameter_sanitizer.permit :sing_up, keys: [:email, :name, :password, :password_confirmation]
-        devise_parameter_sanitizer.permit :account_update, keys: [:email, :name, :password, :password_confirmation]
-        devise_parameter_sanitizer.permit :sign_in, keys: [:email, :name, :password, :password_confirmation]
-    end
     def after_sign_in_path_for(resource)
-        user_path(@user.id)
+        user_path(current_user)
     end
-
+    
     def after_sign_up_path_for(resource)
-        user_path(@user.id)
+        user_path(current_user)
     end
     def after_sign_out_path_for(resource)
         home_path
+    end
+    protected
+    def configure_permitted_parameters
+        # devise_parameter_sanitizer.permit(:sing_up, keys:[:name, :email]) これだと入力できない
+        # devise_parameter_sanitizer.permit(:sign_up, keys:[:name, :email])
+        devise_parameter_sanitizer.permit(:sign_up, keys:[:name, :email])
+        devise_parameter_sanitizer.permit(:sign_in, keys:[:name])
     end
 
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
     before_action :authenticate_user!
+    before_action :correct_user, only:[:edit, :update]
     def create
         @book = Book.new(book_params)
         @book.user_id = current_user.id
@@ -32,11 +33,15 @@ class BooksController < ApplicationController
 
     def edit
         @book = Book.find(params[:id])
+
     end
     def update
         @book = Book.find(params[:id])
-        @book.update(book_params)
-        redirect_to books_path
+        if @book.update(book_params)
+            redirect_to book_path, notice: 'You have updated book successfully'
+        else
+            render :edit
+        end
     end
 
 
@@ -45,6 +50,11 @@ class BooksController < ApplicationController
 
     def book_params
         params.require(:book).permit(:title, :body)
+    end
+
+    def correct_user
+        @book = Book.find(params[:id])
+        redirect_to(books_path) unless @book.user_id == current_user.id
     end
 
 end
